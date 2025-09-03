e66ff83b98a2a0e09c1d7a4127d26adc3a43888a
Santiago <santiago.castillo.silva211103@gmail.com>
2025-09-03 16:36:23 -0500

    1era estructura

---
+++ project/api/src/entrenador.py
@@ -0,0 +1,38 @@
+import cv2
+import os
+import numpy as np
+
+dataPath = '../Fotos' #Cambia a la ruta donde hayas almacenado Data
+modelPath = '../Modelos'
+peopleList = os.listdir(dataPath)
+print('Lista de personas: ', peopleList)
+
+
+if not os.path.exists(modelPath):
+	print('Carpeta creada: ',modelPath)
+	os.makedirs(modelPath)
+
+labels = []
+facesData = []
+label = 0
+
+for nameDir in peopleList:
+	personPath = dataPath + '/' + nameDir
+	print('Leyendo las imágenes')
+
+	for fileName in os.listdir(personPath):
+		print('Rostros: ', nameDir + '/' + fileName)
+		labels.append(label)
+		facesData.append(cv2.imread(personPath+'/'+fileName,0))
+
+	label = label + 1
+	
+face_recognizer = cv2.face.LBPHFaceRecognizer_create()
+
+print("Entrenando...")
+print(np.array(labels))
+face_recognizer.train(facesData, np.array(labels))
+
+face_recognizer.write('../Modelos/modeloLBPHFace.xml')
+
+print("Modelo almacenado...")
